66e5f36bf35454706b439bb984b66aae2d608d2a
d_wisarut <d.wisarut@hotmail.com>
2025-11-27 16:55:22 +0700

    feat: add blogs animation

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,7 +1,40 @@
+import { easeInOut, useAnimation, motion } from "framer-motion";
+import { useEffect } from "react";
+
 const Blog = () => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.set({ opacity: 0, y: -20 });
+    controls.start({
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.2, ease: easeInOut, delay: 0.2 },
+    });
+
+    const handleExit = () => {
+      controls.start({
+        x: "100vh",
+        opacity: 0,
+        transition: { duration: 0.4, ease: easeInOut },
+      });
+    };
+    window.addEventListener("to-home", handleExit);
+    return () => window.removeEventListener("to-home", handleExit);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
-      <div className="flex bg-background min-h-screen w-full p-4"></div>
+      <motion.div
+        className="flex container flex-col bg-background min-h-screen w-full p-8 lg:p-4"
+        animate={controls}
+        initial={{ opacity: 0, y: -20 }}
+      >
+        <h1 className="flex header-text justify-center md:justify-start font-bold pt-6">
+          Blogs
+        </h1>
+      </motion.div>
       <footer className="footer sm:footer-horizontal bg-[hsl(0_0%_10%)] text-[hsl(0_0%_90%)] items-center justify-around mt-6 p-12">
         <aside className="flex flex-col items-start">
           <h1 className="text-lg sm:text-xl lg:text-2xl font-bold">
