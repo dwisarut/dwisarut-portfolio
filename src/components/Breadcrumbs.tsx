8ca237ad2314f0b5d5df69fb4fdfeb8ebd3b9eda
d_wisarut <d.wisarut@hotmail.com>
2025-11-28 17:19:30 +0700

    feat: add adaptive navigation

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,24 +1,61 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-interface Items {
-  page: string;
-  href: string;
+interface metaData {
+  title: string;
+  synopsis: string;
+  type: "project" | "blog";
+  slug: string;
+  tag: string[];
+  cover: string;
+  component: React.FC;
 }
 
-const breadcrumbItems: Items[] = [
-  { page: "Project", href: "/project" },
-  { page: "Blog", href: "/blog" },
-];
+interface breadcrumbProp {
+  allProjects?: metaData[];
+  allBlogs?: metaData[];
+}
 
-const Breadcrumbs = () => {
+const Breadcrumbs = ({ allProjects = [], allBlogs = [] }: breadcrumbProp) => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const { slug } = useParams();
+  const crumbs: { page: string; href: string }[] = [];
+
+  if (
+    !location.pathname.startsWith("/project") &&
+    !location.pathname.startsWith("/blog")
+  ) {
+    return null;
+  }
+
+  if (location.pathname.startsWith("/project")) {
+    crumbs.push({ page: "Project", href: "/project" });
+
+    if (slug) {
+      const project = allProjects.find((p) => p.slug === slug);
+      if (project) {
+        crumbs.push({ page: project.title, href: `/project/${slug}` });
+      }
+    }
+  }
+
+  if (location.pathname.startsWith("/blog")) {
+    crumbs.push({ page: "Blog", href: "/blog" });
+
+    if (slug) {
+      const blog = allBlogs.find((p) => p.slug === slug);
+      if (blog) {
+        crumbs.push({ page: blog.title, href: `/blog/${slug}` });
+      }
+    }
+  }
 
   return (
     <>
       <div className="w-full breadcrumbs text-sm md:text-base text-[hsl(0_0%_40%)]">
         <ul>
-          {breadcrumbItems.map((item) => {
+          {crumbs.map((item) => {
             const currPage = location.pathname === item.href;
 
             return (
