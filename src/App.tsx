bf5dad145ccc4aa3b58fb581de5c803f26b4aeed
d_wisarut <d.wisarut@hotmail.com>
2025-11-29 23:42:07 +0700

    feat: add route to BlogDetail

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Work from "./components/Work";
 import Blog from "./components/Blog";
+import BlogDetail from "./components/BlogDetail";
 import Project from "./components/Project";
 import ProjectDetail from "./components/ProjectDetail";
 
@@ -21,6 +22,7 @@ function App() {
             <Route path="/project" element={<Project />} />
             <Route path="/project/:slug" element={<ProjectDetail />} />
             <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogDetail />} />
           </Routes>
         </div>
       </BrowserRouter>
