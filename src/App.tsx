a4e4f4ea863e49a29e2a9a9e8c84743724edc3f9
d_wisarut <d.wisarut@hotmail.com>
2026-01-20 09:12:54 +0700

    refactor: remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-//import { useState } from "react";
 import LandingPage from "./components/Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Work from "./components/Work";
